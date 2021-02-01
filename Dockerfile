e5e912c071e4e861ccda59df2f0a59f67921ef32
Alexander <info@gruessung.eu>
2021-02-01 11:59:43 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM php:7.3
+
+RUN mkdir /app
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y git curl libc-client-dev libkrb5-dev && rm -r /var/lib/apt/lists/*
+RUN docker-php-ext-configure imap --with-kerberos --with-imap-ssl \
+    && docker-php-ext-install imap
+
+RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+RUN    php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+RUN    php composer-setup.php
+RUN    php -r "unlink('composer-setup.php');"
+
+COPY  index.php index.php
+COPY composer.json composer.json
+#COPY .env .env
+
+RUN php composer.phar update
+CMD ["php", "/app/index.php"]
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+require_once('vendor/autoload.php');
+
+if (file_exists('.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
+//Check .env Variables
+function setEnvDefault($envName, $envRequired, $envDefault = '')
+{
+    if ( (getenv($envName) == false && !isset($_ENV[$envName])) && $envRequired === true) {
+        throw new Exception('ENV missing: ' . $envName);
+    } else if (!isset($_ENV[$envName])) {
+        $_ENV[$envName] = $envDefault;
+    }
+}
+try {
+    setEnvDefault('LO_API_KEY', true);
+    setEnvDefault('IMAP_HOST', true);
+    setEnvDefault('IMAP_USER', true);
+    setEnvDefault('IMAP_PASS', true);
+    setEnvDefault('IMAP_PORT', false, '993');
+    setEnvDefault('IMAP_PATH', false, '/imap/ssl');
+    setEnvDefault('IMAP_FOLDER', false, 'INBOX');
+    setEnvDefault('DELETE_MAIL', false, 'false');
+} catch (Exception $e) {
+    die($e->getMessage().PHP_EOL);
+}
+
+$attachmetsDir = __DIR__ . DIRECTORY_SEPARATOR . 'attachments';
+
+if (!is_dir($attachmetsDir)) {
+    mkdir($attachmetsDir, 0777, true);
+}
+
+// Create PhpImap\Mailbox instance for all further actions
+$mailbox = new PhpImap\Mailbox(
+    sprintf('{%s:%s%s}%s', $_ENV['IMAP_HOST'], $_ENV['IMAP_PORT'], $_ENV['IMAP_PATH'], $_ENV['IMAP_FOLDER']), // IMAP server and mailbox folder
+    $_ENV['IMAP_USER'], // Username for the before configured mailbox
+    $_ENV['IMAP_PASS'], // Password for the before configured username
+    $attachmetsDir, // Directory, where attachments will be saved (optional)
+    'UTF-8' // Server encoding (optional)
+);
+
+// set some connection arguments (if appropriate)
+$mailbox->setConnectionArgs(
+    CL_EXPUNGE // don't do non-secure authentication
+);
+
+while (true) {
+
+    try {
+        $mailsIds = $mailbox->searchMailbox('UNSEEN');
+    } catch (PhpImap\Exceptions\ConnectionException $ex) {
+        throw new Exception("IMAP connection failed: " . $ex->getMessage());
+    }
+
+    if (!$mailsIds) {
+        echo '[LOG] Mailbox is empty...wait 2 seconds' . PHP_EOL;
+        sleep(2);
+        continue;
+    }
+
+    foreach ($mailsIds as $mailId) {
+        $mail = $mailbox->getMail($mailId);
+
+        echo '[MAIL] Found mail from '.$mail->fromAddress.PHP_EOL;
+
+        if ($mail->hasAttachments()) {
+
+            $attachments = $mail->getAttachments();
+
+            echo '[INFO] Found '. count($attachments). ' attachments'.PHP_EOL;
+
+            //send to lexoffice :)
+            foreach ($attachments as $attachment) {
+                rename($attachment->filePath, str_replace('.bin', '.pdf', $attachment->filePath));
+                $attachmentPath = str_replace('.bin', '.pdf', $attachment->filePath);
+                exec('curl https://api.lexoffice.io/v1/files -X POST -H "Authorization: Bearer '.$_ENV['LO_API_KEY'].'" -H "Content-Type: multipart/form-data" -H "Accept: application/json" -F "file=@' . $attachmentPath . '" -F "type=voucher"');
+            }
+        } else {
+            echo '[INFO] Mail has no attachments' . PHP_EOL;
+        }
+
+        if ($_ENV['DELETE_MAIL'] == 'true') {
+            $mailbox->deleteMail($mail->id);
+            echo '[INFO] Mail gelöscht'.PHP_EOL;
+            $mailbox->expungeDeletedMails();
+
+        }
+    }
+}
+
